53e7556ca3ce8be91f17fb0bd6cb77f93d650855
JeongYuJin <wjddbwls1230@naver.com>
2025-02-15 09:02:53 +0900

    [Silver IV] Title: 등수 구하기, Time: 104 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Silver/1205. 등수 구하기/등수 구하기.java
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.java
@@ -2,8 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-import java.util.ArrayList;
-import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -14,36 +12,30 @@ public class Main {
         long newScore = Long.parseLong(st.nextToken());
         int p = Integer.parseInt(st.nextToken());
 
-        if (n == 0) {
+        if (n == 0) { 
             System.out.println("1");
-            return;
-        }
+        } else {
+            long[] scores = new long[n];
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int i = 0; i < n; i++) {
+                scores[i] = Long.parseLong(st.nextToken());
+            }
 
-        ArrayList<Long> scores = new ArrayList<>();
-        st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < n; i++) {
-            scores.add(Long.parseLong(st.nextToken()));
-        }
-
-        // 리스트 정렬 (내림차순)
-        Collections.sort(scores, Collections.reverseOrder());
-
-        // 리스트 크기가 랭킹 제한을 넘어서면 마지막 요소를 삭제
-        if (n == p && scores.get(n - 1) >= newScore) {
-            System.out.println("-1");
-            return;
-        }
-
-        // 새로운 점수의 랭킹 계산
-        int rank = 1;
-        for (long score : scores) {
-            if (score > newScore) {
-                rank++;
+            // 리스트 크기가 `p`개를 꽉 채운 상태에서 마지막 점수보다 작거나 같으면 `-1` 출력
+            if (n == p && scores[n - 1] >= newScore) {
+                System.out.println("-1");
             } else {
-                break;
+                // 새로운 점수의 랭킹 계산
+                int rank = 1;
+                for (int i = 0; i < n; i++) {
+                    if (scores[i] > newScore) {
+                        rank++;
+                    } else {
+                        break;
+                    }
+                }
+                System.out.println(rank);
             }
         }
-
-        System.out.println(rank);
     }
 }
