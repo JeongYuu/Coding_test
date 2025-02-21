6c228c586bb90af335c11f4b74d3365d965e9275
JeongYuJin <wjddbwls1230@naver.com>
2025-02-22 05:39:33 +0900

    [Silver III] Title: 주유소, Time: 308 ms, Memory: 30568 KB, Score: 58 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -14,16 +12,12 @@ public class Main {
         int N = Integer.parseInt(str);
         int[] km = new int[N-1];
         int[] city = new int[N];
-        int[] grade = new int[N-1];
-        int sum = 0;
-        HashMap<Integer,Integer> hm = new HashMap<>();
 
         str = br.readLine();
         st = new StringTokenizer(str, " ");
 
         for(int i=0;i<N-1;i++) {
             km[i] = Integer.parseInt(st.nextToken());
-            sum += km[i];
         }
 
         str = br.readLine();
@@ -33,32 +27,13 @@ public class Main {
             city[i] = Integer.parseInt(st.nextToken());
         }
 
-        for(int i=0;i<N-1;i++) {
-            grade[i] = city[i];
-        }
-
-        Arrays.sort(grade);
-
-        int num=1;
-
-        for(int i=0;i<N-1;i++) {
-            hm.putIfAbsent(grade[i],num++);
-        }
-
+        int min = city[0];
         int answer = 0;
 
         for(int i=0;i<N-1;i++){
-            if(hm.get(city[i])!=1){
-                answer += km[i]*city[i];
-                sum -= km[i];
-            }
-            else{
-                answer += sum*city[i];
-                break;
-            }
+            if(min>city[i]) min = city[i];
+            answer += min*km[i];
         }
-
         System.out.println(answer);
-
     }
 }
